0a011175ec9bf532968b060cc506e81c031ec170
Benoît Mallo <benoit.mallo@gmail.com>
2017-09-30 07:54:39 +0000

    ADD Adding the is-invalid class to the field that is in error.

--- Form/Builder/EntityFormBuilder.php
+++ Form/Builder/EntityFormBuilder.php
@@ -28,6 +28,11 @@ class EntityFormBuilder
     private $session;
 
     /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
      * EntityFormBuilder constructor.
      *
      * @param FormBuilder $formBuilder
@@ -37,6 +42,7 @@ class EntityFormBuilder
 	{
 		$this->formBuilder = $formBuilder;
         $this->session = $session;
+        $this->errors = $this->getErrors();
     }
 
 	/**
@@ -54,56 +60,10 @@ class EntityFormBuilder
 		}
 		$this->formBuilder->openForm($action, 'POST');
 		foreach (array_keys(get_object_vars($entity)) as $property) {
-			$propertyAnnotation = new ReflectionAnnotation($entity, $property);
 			$divElement = new Element('div');
 			$divElement->addClass('form-group');
 			$this->formBuilder->add($divElement);
-			$errors = $this->getErrors();
-			if (
-				$propertyAnnotation->hasAnnotation('var')
-				&& $propertyAnnotation->getAnnotation('var')->getValue() === Type::STRING
-			) {
-				$value = null;
-                if (property_exists(get_class($entity), $property) && $entity->get($property)) {
-					$value = $entity->get($property);
-				}
-                $input = new Input($property, $value);
-                $input->addClass('form-control');
-                if (isset($errors[$property]) && !empty($errors[$property])) {
-                    $input->addClass('is-invalid');
-                    $this->session->delete('errors');
-                } else {
-                    $input->removeClass('is-invalid');
-                }
-				if ($propertyAnnotation->getPropertyAnnotation(Option::class)) {
-					$optionAnnoation = $propertyAnnotation->getPropertyAnnotation(Option::class);
-					$type = $optionAnnoation->type ? $optionAnnoation->type : Type::TEXT;
-					if ($optionAnnoation->mandatory) {
-						$input->setAttribute('required');
-					}
-					if ($optionAnnoation->placeholder) {
-					    $input->setAttribute('placeholder', $optionAnnoation->placeholder);
-                    }
-				}
-				else {
-					$type = Type::TEXT;
-				}
-				$input->setAttribute('type', $type);
-				$this->formBuilder->add($input);
-			}
-
-			if (
-				$propertyAnnotation->hasAnnotation('var')
-				&& $propertyAnnotation->getAnnotation('var')->getValue() === Type::TEXT
-			) {
-				$value = null;
-				if (property_exists(get_class($entity), $property) && $entity->get($property)) {
-					$value = $entity->get($property);
-				}
-				$textarea = new Textarea($property, $value);
-				$textarea->setAttribute('class', 'form-control');
-				$this->formBuilder->add($textarea);
-			}
+			$this->addField($entity, $property);
             $this->formBuilder->add('</div>');
 		}
 		$button = new Button('Envoyer');
@@ -119,6 +79,87 @@ class EntityFormBuilder
 	private function getErrors(): array
     {
         return $this->session->get('errors') ?? [];
+    }
+
+    /**
+     * Ajoute la classe error au champ en erreur et la description de l'erreur dans une div.
+     *
+     * @param Element $element Le champ qui est en erreur
+     * @param string $property
+     * @return Element
+     */
+    private function addErrorField(Element $element, string $property): ?Element
+    {
+        $span = null;
+        if (isset($this->errors[$property]) && !empty($this->errors[$property])) {
+            $element->addClass('is-invalid');
+            $span = new Element('div', true);
+            $span->addClass('invalid-feedback');
+            $span->setContent(implode(', ', $this->errors[$property]));
+            $this->session->delete('errors');
+        } else {
+            $element->removeClass('is-invalid');
+        }
+        return $span;
+    }
+
+    /**
+     * @param Entity $entity
+     * @param string $property
+     */
+    private function addField(Entity $entity, string $property): void
+    {
+        $propertyAnnotation = new ReflectionAnnotation($entity, $property);
+        if (
+            $propertyAnnotation->hasAnnotation('var')
+            && $propertyAnnotation->getAnnotation('var')->getValue() === Type::STRING
+        ) {
+            $value = null;
+            if (property_exists(get_class($entity), $property) && $entity->get($property)) {
+                $value = $entity->get($property);
+            }
+            $input = new Input($property, $value);
+            $input->addClass('form-control');
+            if ($propertyAnnotation->getPropertyAnnotation(Option::class)) {
+                $optionAnnoation = $propertyAnnotation->getPropertyAnnotation(Option::class);
+                $type = $optionAnnoation->type ? $optionAnnoation->type : Type::TEXT;
+                if ($optionAnnoation->mandatory) {
+                    $input->setAttribute('required');
+                }
+                if ($optionAnnoation->placeholder) {
+                    $input->setAttribute('placeholder', $optionAnnoation->placeholder);
+                }
+            }
+            else {
+                $type = Type::TEXT;
+            }
+            $input->setAttribute('type', $type);
+            $span = $this->addErrorField($input, $property);
+            $this->formBuilder->add($input . $span);
+        }
+
+        if (
+            $propertyAnnotation->hasAnnotation('var')
+            && $propertyAnnotation->getAnnotation('var')->getValue() === Type::TEXT
+        ) {
+            $value = null;
+            if (property_exists(get_class($entity), $property) && $entity->get($property)) {
+                $value = $entity->get($property);
+            }
+            $textarea = new Textarea($property, $value);
+            if ($propertyAnnotation->getPropertyAnnotation(Option::class)) {
+                $optionAnnoation = $propertyAnnotation->getPropertyAnnotation(Option::class);
+                if ($optionAnnoation->mandatory) {
+                    $textarea->setAttribute('required');
+                }
+                if ($optionAnnoation->placeholder) {
+                    $textarea->setAttribute('placeholder', $optionAnnoation->placeholder);
+                }
+            }
+            $textarea->setAttribute('class', 'form-control');
+            $this->formBuilder->add($textarea);
+            $this->addErrorField($textarea, $property);
+        }
     }
 
 }
